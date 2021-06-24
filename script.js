83a4e1ad71504c8c4e9a3341e453ec53617a0694
KhaledShehataA <khaled.ah.shehata@gmail.com>
2021-06-24 17:55:17 +0200

    refactor: check required fields

--- script.js
+++ script.js
@@ -3,54 +3,45 @@ const username = document.getElementById('username');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-
-
 // Show input error message
 function showError(input, message) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
-    small.innerText = message;
+	const formControl = input.parentElement;
+	formControl.className = 'form-control error';
+	const small = formControl.querySelector('small');
+	small.innerText = message;
 }
 
 // Show success outline
 function showSuccess(input) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success'
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
 }
 
 // Show email is valid
 function isValidEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
 }
 
-form.addEventListener('submit' , function(e) {
-    e.preventDefault();
+// Check required fields
+function checkRequired(inputArr) {
+	inputArr.forEach((input) => {
+		if (input.value.trim() === '') {
+			showError(input, `${getFieldName(input)} is required`);
+		} else {
+			showSuccess(input);
+		}
+	});
+}
 
-    if(username.value === '') {
-        showError(username, 'Username is required');
-    } else {
-        showSuccess(username);
-    }
+// Get fieldname
+function getFieldName(input) {
+	return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 
-    if(email.value === '') {
-        showError(email, 'Email is required');
-    } else if(!isValidEmail(email.value)) {
-        showError(email, 'Email is not valid')
-    } else {
-        showSuccess(email);
-    }
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
 
-    if(password.value === '') {
-        showError(password, 'Password is required');
-    } else {
-        showSuccess(password);
-    }
-
-    if(password2.value === '') {
-        showError(password2, 'Password 2 is required');
-    } else {
-        showSuccess(password2);
-    }
+	checkRequired([username, email, password, password2]);
 });
